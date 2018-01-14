3d5acaa28c3aecb6b4cb725b82ef1f0bc2052f97
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-01-14 17:46:05 +0100

    some additions to test.m, as well as two function just to output membrane potential without plotting it

--- VCN_freq.m
+++ VCN_freq.m
@@ -1,4 +1,4 @@
-function [ ] = VCN( I_stim1, freq1, I_stim2, freq2, I_stim3, freq3,...
+function [ V4 ] = VCN( I_stim1, freq1, I_stim2, freq2, I_stim3, freq3,...
                     T, f_s, slope, type )
 %VCN_SPIKE Summary of this function goes here
 %   Detailed explanation goes here
@@ -67,24 +67,11 @@ ramp               = ones(1,numel(I));
 ramp(1:Tonset*f_s) = 0;
 ramp_val           = 0:slope:1;
 ramp(Tonset*f_s+1:Tonset*f_s+numel(ramp_val)) = ramp_val;
-figure; plot(t*1000, ramp);
 
 % size(ramp)
 I = I .* ramp;
 
 V4 = VCN_I(1e-9*I,  g_Na,g_HT,g_LT,g_A,g_h,g_lk,V_0, f_s);
-
-figure;
-subplot(5,1,[1, 2]); plot(t*1000, I); ylim([min(I)-.01,max(I)+.01]); grid; % ylim([-100 100]);
-legend('Stimulation Current', 'Location', 'northwest');
-ylabel({'$I (nA)$'},'Interpreter','latex');
-
-subplot(5,1,[3, 4, 5]); plot(t*1000, V4*1000); grid; % ylim([-100, 60]);
-legend('Action Potential', 'Location', 'southwest')
-xlabel({'$Time (ms)$'},'Interpreter','latex');
-ylabel({'$Membrane voltage (mV)$'},'Interpreter','latex');
-
-suptitle({['Type ', type, ' Model'], '(Phasic Neuron)'});
 
 end
 
