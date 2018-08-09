38f66cfb47c68aef803a267ce1a2b0c61d355c34
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-08-09 14:13:39 +0200

    last commit before modifying the VCN model matlab code

--- Matlab/Scripts/figures.m
+++ Matlab/Scripts/figures.m
@@ -1,32 +1,31 @@
-%% MHH model simulation
+close all; clear all;
+%
+% This script is used to generate figures for the paper 
+% 
+% Author: Mohammad Bashiri
+% Date (Last Update): 09/08/2018
+%  
+%
+%% VCN model simulation
+
+clear all
 
 % initializing simulation param
-tend = 20000;
-fs   = 100;
-dt   = 1/fs; % 0.01
-t = 0:dt:tend-dt;
+tend = 200e-3; % 2sec
+fs   = 1e6;
 
-slope = 1000e-5; % this is in the units of the current over time!
-stim_freq = 1.0; % kHz
-
-I_stim = 32;  %31.45;
+neuron_type = 'II';
+slope = 1; %.00005;
+stim_freq = 3000;
 
 % initialize stimulaiton current
-I_stim1 = I_stim / 2;    % nA 
-freq1   = stim_freq;     % 0.01 => 10 Hz
+I_stim1 = 5;    % nA 
+freq1   = stim_freq;    % Hz
 
-I_stim2 = I_stim / 2 * 0;    % nA 
-freq2   = stim_freq + .0;    % 0.01 => 10 Hz
+I_stim2 = 5;     % nA 
+freq2   = stim_freq + 30;  % Hz
 
-I_stim3 = 0;       % nA 
-freq3   = stim_freq + .02;    % 0.01 => 10 Hz
+I_stim3 = 0;     % nA 
+freq3   = stim_freq + 1.02;  % Hz
 
-
-% plot(t/1000, chirp(t, 0.0001, tend, .02))
-MHH( I_stim1, freq1, I_stim2, freq2, I_stim3, freq3, tend, dt, slope );
-
-% xticks(0:20:tend);
-% xticklabels({round(linspace(f_start,f_end, length(0:20:tend)))});
-% xlabel('Frequency [Hz]');
-
-%% VCN simulation
+VCN( I_stim1, freq1, I_stim2, freq2, I_stim3, freq3, tend, fs, slope, neuron_type );
