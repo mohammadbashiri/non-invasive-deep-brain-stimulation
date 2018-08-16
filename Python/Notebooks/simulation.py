803ab0a02777667d24e8f52f2509214e4e882fc9
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-08-16 15:48:37 +0200

    refactored the Simulation class and now we can say Sim.run() and it will run the simulation by updating the state of tracked neuron attributes in every iteration

--- Python/Notebooks/simulation.py
+++ Python/Notebooks/simulation.py
@@ -1,10 +1,22 @@
 import numpy as np
 
 class Simulation(object):
-    def __init__(self, Sim_period, Sim_fs):
+    def __init__(self, period, fs):
         
-        self.period = 5000 # milliseconds
-        self.fs = 200  # samples per second
+        self.period = period # milliseconds
+        self.fs = fs  # samples per second
 
-        self.dt = 1/Sim_fs
+        self.dt = 1/self.fs
         self.t = np.arange(0, self.period, self.dt)
+
+        self.neuron = None
+        self.stim = None
+
+    def run(self):
+    	"""Runs the simulation given a neuron mdoel and stimulation signal"""
+    	if self.neuron and self.stim:
+	     	for i in range(self.stim.shape[0]-1):
+    	 		self.neuron.update(self.stim[i], i, self.dt)
+
+    	else:
+    	 	raise AttributeError("Make sure you have included your neuron model and stimulation signal to the simulation objcet. \n Example: \n\tmySim.neuron = myNeuron \n\tmySim.stim = I")
